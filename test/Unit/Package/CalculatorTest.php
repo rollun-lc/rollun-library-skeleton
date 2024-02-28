092485361deeb5d38b9ab0547e0b39d3af1376d8
Mykhailo Zuiev <zuiev.rollun@gmail.com>
2024-02-28 14:36:42 +0200

    refactor(e30CjZne): increase php minimum version to 7.2

--- test/Unit/Package/CalculatorTest.php
+++ test/Unit/Package/CalculatorTest.php
@@ -7,7 +7,7 @@ use Rollun\Package\Calculator;
 
 class CalculatorTest extends TestCase
 {
-    public function testSum()
+    public function testSum(): void
     {
         $calculator = new Calculator();
         self::assertEquals(4, $calculator->sum(2, 2));

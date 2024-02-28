cda7382c4b77a363c39f7671c0a094c53fd7fac9
Mykhailo Zuiev <zuiev.rollun@gmail.com>
2024-02-28 14:33:17 +0200

    fix(e30CjZne): autoload namespaces

---
+++ src/Package/src/Calculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rollun\Package;
+
+class Calculator
+{
+    public function sum(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+}
---
+++ test/Unit/Package/CalculatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rollun\Test\Unit\Package;
+
+use PHPUnit\Framework\TestCase;
+use Rollun\Package\Calculator;
+
+class CalculatorTest extends TestCase
+{
+    public function testSum()
+    {
+        $calculator = new Calculator();
+        self::assertEquals(4, $calculator->sum(2, 2));
+    }
+}
